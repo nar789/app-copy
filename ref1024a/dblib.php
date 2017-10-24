27b148b81e868dfc400c0bc8326185be1ce63da0
nar789 <tony92511@hanmail.net>
2017-10-24 14:48:29 +0900

    1024

---
+++ ref1024a/dblib.php
@@ -0,0 +1,9 @@
+<?
+	$db_host ="localhost";
+	$db_user="iop0907";
+	$db_passwd="top41567747";
+	$db_name="iop0907";
+	$servername="ref";
+	$appname=array('데이트클럽','막장채팅','불타는채팅','비밀연애','엔조이닷컴');
+?>
+
---
+++ ref1024b/dblib.php
@@ -0,0 +1,9 @@
+<?
+	$db_host ="localhost";
+	$db_user="iop0909";
+	$db_passwd="top41567747";
+	$db_name="iop0909";
+	$servername="ref";
+	$appname=array('유부클럽','은밀한연애','카사노바톡','프리데이','하루메이트');
+?>
+
