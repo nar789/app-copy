3761420dd59cf3a5a3c7ba43bda42549ad1847a2
nar789 <tony92511@hanmail.net>
2017-11-21 12:13:24 +0900

    chg package name

--- 1024cordovaApps/1/platforms/android/src/cpp/holo/i2de7a/MainActivity.java
+++ 1024cordovaApps/1/platforms/android/src/cpp/holo/i2de7a/MainActivity.java
@@ -17,7 +17,7 @@
        under the License.
  */
 
-package rucas.cd1uk;
+package cpp.holo.i2de7a;
 
 import android.os.Bundle;
 import org.apache.cordova.*;
--- 1024cordovaApps/2/platforms/android/src/toy/cay/r3u5st/MainActivity.java
+++ 1024cordovaApps/2/platforms/android/src/toy/cay/r3u5st/MainActivity.java
@@ -17,7 +17,7 @@
        under the License.
  */
 
-package rtroy.de0ps;
+package toy.cay.r3u5st;
 
 import android.os.Bundle;
 import org.apache.cordova.*;
